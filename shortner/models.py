978275d12ae00ee305701b6b9fb4da5a9156b863
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-04-14 19:16:07 +0530

    Set up models and basic frontend with AJAX call.

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MyModel(models.Model):
+    """Models for database"""
+    link = models.CharField(max_length=1000)
+    unique_id = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.link
